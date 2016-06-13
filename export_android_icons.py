d54ceff3bc5c4287b746d5aaa42ade5754963400
leandrobpedro <leandrobpedro@gmail.com>
2016-06-13 17:33:23 -0300

    Serveral changes

--- export_android_icons.py
+++ export_android_icons.py
@@ -10,6 +10,41 @@ try:
 except ImportError:
   DEVNULL = open(os.devnull, 'w')
 
+class SizeGroup(optparse.OptionGroup):
+    def add_size_option(self, name, size, dpi):
+        self.add_option('--%s' % name, action='callback', type='string', dest='sizes',
+                callback=append_size, callback_args=(name, size, dpi))
+
+class ExportAndroidIcons(inkex.Effect):
+
+    def __init__(self):
+        inkex.Effect.__init__(self)
+        self.OptionParser.add_option('--filename', action='store', type='string', dest='filename')
+        self.OptionParser.add_option('--directory', action='store',type='string', dest='directory')
+        self.OptionParser.add_option('--radio', action='store', type='string', dest='radio')
+
+        group = SizeGroup(self.OptionParser, '')
+        group.add_size_option('ldpi', 32, 67.5)
+        group.add_size_option('mdpi', 48, 90)
+        # group.add_size_option('tvdpi', 64, 120)
+        group.add_size_option('hdpi', 72, 135)
+        group.add_size_option('xhdpi', 96, 180)
+        group.add_size_option('xxhdpi', 144, 270)
+        group.add_size_option('xxxhdpi', 192, 360)
+        group.add_size_option('web', 512, 960)
+
+        self.OptionParser.add_option_group(group)
+
+        (self.options, self.args) = self.OptionParser.parse_args()
+
+    def effect(self):
+        doc = self.document.getroot()
+        svg = self.args[0]
+        width  = self.unittouu(doc.get('width'))
+        height = self.unittouu(doc.get('height'))
+
+        export(self.options, svg)
+
 def str_to_bool(s):
     if s.lower() == 'true':
          return True
@@ -20,62 +55,42 @@ def create_folder(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
-def append_size(option, opt_str, value, parser, *size):
+def append_size(option, opt_str, value, parser, *size, **dpi):
     if not str_to_bool(value):
         return
     if getattr(parser.values, option.dest) is None:
         setattr(parser.values, option.dest, [])
     getattr(parser.values, option.dest).append(size)
 
-class SizeGroup(optparse.OptionGroup):
-    def add_size_option(self, name, size):
-        self.add_option('--%s' % name, action='callback', type='string', dest='sizes',
-                callback=append_size, callback_args=(name, size))
+def error(msg):
+    inkex.debug(msg)
+    sys.exit(1)
 
+def export(options, svg):
+    if not options.filename:
+        error('The icon name must be non-null.')
+    if not options.directory:
+        error('No destination directory selected.')
+    if not options.sizes:
+        error('Please, select at least one option to export.')
 
-class ExportAndroidIcons(inkex.Effect):
+    for qualifier, size, dpi in options.sizes:
+        # For web icons, do not use subfolder, create on directory root.
+        subfolder = '%s-%s' % (options.radio, qualifier) if qualifier != 'web' else ''
+        path = '%s\%s' % (options.directory, subfolder)
+        png = path + '\%s.png' % options.filename
 
-    def __init__(self):
+        # Size base icons for mipmap, and dpi for drawable.
+        opt, val = ('width', size) if (options.radio == 'mipmap') else ('dpi', dpi)
 
-        inkex.Effect.__init__(self)
-        self.OptionParser.add_option('--filename', action='store', type='string', dest='filename')
-        self.OptionParser.add_option('--directory', action='store',type='string', dest='directory', default='c:')
-        self.OptionParser.add_option('--radio', action='store', type='string', dest='radio')
+        create_folder(path);
 
-        group = SizeGroup(self.OptionParser, '')
-        group.add_size_option('ldpi', 32)
-        group.add_size_option('mdpi', 48)
-        # group.add_size_option('tvdpi', 64)
-        group.add_size_option('hdpi', 72)
-        group.add_size_option('xhdpi', 96)
-        group.add_size_option('xxhdpi', 144)
-        group.add_size_option('xxxhdpi', 192)
-        group.add_size_option('web', 512)
-        self.OptionParser.add_option_group(group)
-
-        (self.options, self.args) = self.OptionParser.parse_args()
-
-    def effect(self):
-        doc = self.document.getroot()
-        svg = self.args[0]
-        width  = self.unittouu(doc.get('width'))
-        height = self.unittouu(doc.attrib['height'])
-
-        for qualifier, size in self.options.sizes:
-            subfolder = '%s-%s' % (self.options.radio, qualifier) if qualifier != 'web' else ''
-            path = '%s\%s' % (self.options.directory, subfolder)
-            png = path + '\%s.png' % self.options.filename
-
-            create_folder(path)
-
-            subprocess.check_call([
-            		'inkscape',
-            		'--export-area-page',
-            		# '--export-dpi=%s' % dpi,
-                    '--export-width=%s' % size,
-                    '--export-height=%s' % size,
-            		'--export-png=%s' % png,
-            		svg])
+        subprocess.check_call([
+                'inkscape',
+                '--export-area-page',
+                '--export-%s=%s' % (opt, val),
+                '--export-png=%s' % png,
+                svg])
 
 effect = ExportAndroidIcons()
 effect.affect()
